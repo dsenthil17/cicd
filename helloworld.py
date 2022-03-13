c2b90a696438e085988101a7024ec1134d4b77f0
dsenthil17 <79158941+dsenthil17@users.noreply.github.com>
2022-03-13 14:29:15 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,13 @@
-# This program prints Hello, world!
+import argparse
+import sys
 
-print('Hello, world!')
+if __name__=='__main__':
+
+    parser = argparse.ArgumentParser(
+        description="Script for print message",
+        usage='''printmessage.py [-h] -m'''
+    )
+    parser.add_argument('-m', '--input-msg', dest='msg')
+    args = parser.parse_args(sys.argv[1:])
+
+    print(args.msg)
